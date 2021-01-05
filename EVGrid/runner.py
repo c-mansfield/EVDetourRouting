164191bf06921a79d35ef352fb0f09376ea886ff
Cole Mansfield <colemansfield@gmail.com>
2021-01-05 12:42:12 +0000

    Moved EV vtype to main, started re-route algorithm with TRACI, started EV vehicle checks on sim step

--- EVGrid/runner.py
+++ EVGrid/runner.py
@@ -1,5 +1,5 @@
 # from main import run, get_options
-import os,sys,inspect
+import os, sys, inspect
 current_dir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
 parent_dir = os.path.dirname(current_dir)
 sys.path.insert(0, parent_dir)
@@ -17,38 +17,11 @@ from sumolib import checkBinary  # noqa
 import traci  # noqa
 import randomTrips  # noqa
 
-def generate_ev():
-    random.seed(42)  # make tests reproducible
-    # N = 3600  # nmber of time steps
-    with open("data/electricvehicles.rou.xml", "w") as routes:
-        print("""<routes>
-            <vType id="electricvehicle" accel="0.8" decel="4.5" sigma="0.5" minGap="2.5" maxSpeed="40" guiShape="passenger" emissionClass="Energy/unknown">
-                    <param key="has.battery.device" value="true"/>
-                    <param key="maximumBatteryCapacity" value="25"/>
-                    <param key="maximumPower" value="1000"/>
-                    <param key="vehicleMass" value="10000"/>
-                    <param key="frontSurfaceArea" value="5"/>
-                    <param key="airDragCoefficient" value="0.6"/>
-                    <param key="internalMomentOfInertia" value="0.01"/>
-                    <param key="radialDragCoefficient" value="0.5"/>
-                    <param key="rollDragCoefficient" value="0.01"/>
-                    <param key="constantPowerIntake" value="100"/>
-                    <param key="propulsionEfficiency" value="0.9"/>
-                    <param key="recuperationEfficiency" value="0.9"/>
-                    <param key="stoppingTreshold" value="0.1"/>
-            </vType>
-
-            <route id="circuit" edges="gneE0 gneE4 gneE5 gneE6 gneE7 gneE8 gneE9 gneE10"/>
-            """, file=routes)
-        vehNr = 0
-        print('     <vehicle id="EV1" type="electricvehicle" depart="0" route="circuit" ><param key="actualBatteryCapacity" value="5"/></vehicle>', file=routes)
-        print("</routes>", file=routes)
-
 def generate_trips():
     randomTrips.main(randomTrips.get_options([
         '-n', 'data/EVGrid.net.xml',
         '--route-file', 'data/electricvehicles.rou.xml',
-        #'-t', 'data/electricvehicles.trip.xml',
+        '-t', 'data/electricvehicles.trip.xml',
         '--prefix', 'EV',
         '-e', '10',
         '-p', '1.93',
@@ -65,8 +38,9 @@ if __name__ == "__main__":
     else:
         sumoBinary = checkBinary('sumo-gui')
 
-    # Generates electric vehicles in route file
-    generate_ev()
+    # Generates electric vehicle route and random trips
+    main.add_ev_vtype()
+    #generate_trips()
 
     # this is the normal way of using traci. sumo is started as a
     # subprocess and then the python script connects and runs
--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import optparse
+import random
 
 if 'SUMO_HOME' in os.environ:
     tools = os.path.join(os.environ['SUMO_HOME'], 'tools')
@@ -14,13 +15,27 @@ import traci  # noqa
 def run(options=None):
     """execute the TraCI control loop"""
     step = 0
+    route = get_optimal_route_ev()
+
+    traci.route.add('trip', route)
+    traci.vehicle.add('EV1', 'trip', typeID='electricvehicle')
+    traci.vehicle.setParameter('EV1', 'device.battery.actualBatteryCapacity', '200')
+
     while traci.simulation.getMinExpectedNumber() > 0:
         traci.simulationStep()
+
+        # Checks EV battery capacity
         print(traci.vehicle.getParameter('EV1', 'device.battery.actualBatteryCapacity'))
+        if traci.vehicle.getParameter('EV1', 'device.battery.actualBatteryCapacity') == "0.00":
+            print('Vehicle battery empty')
+            #break
+
         step += 1
 
     traci.close()
     sys.stdout.flush()
+
+
 
 # Adds additional info to end of
 def get_options():
@@ -31,3 +46,37 @@ def get_options():
                          default=False, help="Run algorithm on simulation")
     options, args = optParser.parse_args()
     return options
+
+# Generates optimal route for electric vehicle
+def get_optimal_route_ev():
+    #Placeholder route for now
+    return ["gneE0", "gneE4", "gneE5", "gneE6", "gneE7", "gneE8", "gneE9", "gneE10"]
+
+# Adds vehicle type electric vehicle
+def add_ev_vtype():
+    random.seed(42)  # make tests reproducible
+    # N = 3600  # nmber of time steps
+
+    original_stdout = sys.stdout
+
+    with open("data/electricvehicles.rou.xml", "w") as routes:
+         sys.stdout = routes
+         print("""<routes>
+            <vType id="electricvehicle" accel="0.8" decel="4.5" sigma="0.5" minGap="2.5" maxSpeed="40" guiShape="passenger" emissionClass="Energy/unknown">
+                     <param key="has.battery.device" value="true"/>
+                     <param key="maximumBatteryCapacity" value="200"/>
+                     <param key="maximumPower" value="1000"/>
+                     <param key="vehicleMass" value="10000"/>
+                     <param key="frontSurfaceArea" value="5"/>
+                     <param key="airDragCoefficient" value="0.6"/>
+                     <param key="internalMomentOfInertia" value="0.01"/>
+                     <param key="radialDragCoefficient" value="0.5"/>
+                     <param key="rollDragCoefficient" value="0.01"/>
+                     <param key="constantPowerIntake" value="100"/>
+                     <param key="propulsionEfficiency" value="0.9"/>
+                     <param key="recuperationEfficiency" value="0.9"/>
+                     <param key="stoppingTreshold" value="0.1"/>
+             </vType>""")
+         #print('     <vehicle id="EV1" type="electricvehicle" depart="0" route="circuit" ><param key="actualBatteryCapacity" value="5"/></vehicle>')
+         print("</routes>")
+         sys.stdout = original_stdout
